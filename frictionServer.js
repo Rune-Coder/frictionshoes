7812485b4954cdb68b99e3ed7ca9e973aabee834
unknown <souhardya.dutta7801@gmail.com>
2022-11-02 20:15:29 +0530

    mark 6

--- frictionServer.js
+++ frictionServer.js
@@ -32,7 +32,7 @@ app.get("/", (req, res)=> {
     res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production'){
     app.get('/',(req, res) => {
